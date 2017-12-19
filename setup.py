5e994ac6f6331a5ae32963dad32d7eb24cf9c391
Casey McGinty <casey.mcginty@gmail.com>
2017-12-18 20:54:21 -0800

    Workaround PBR parsing bugs

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@
 
 from setuptools import setup  # type: ignore
 
-setup(setup_requires=['pbr'], pbr=True)
+setup(
+    setup_requires=['pbr>=3.1.1', 'setuptools>=38.2.4'],
+    pbr=True,
+    # extra metadata that PBR ignores
+    python_requires='>=3.6',
+    long_description_content_type='text/markdown; charset=UTF-8',
+)
