c5486c3bfba63f4039d98938afdf1b1793ae3e0a
Casey McGinty <casey.mcginty@gmail.com>
2017-12-26 16:12:19 -0800

    Implement TOML config module

--- srm/config.py
+++ srm/config.py
@@ -3,33 +3,114 @@ Manage global and local configuration data.
 """
 
 import os
-from typing import Optional
+from collections import abc
+from typing import Dict  # pylint: disable=unused-import
+from typing import Any, Iterator, List, Optional, Set, Tuple
 
-_GLOBAL_PATH = "~/.srmconfig"
+import toml  # type: ignore
+from boltons import funcutils, iterutils  # type: ignore
+
+# pylint: disable=notimplemented-raised, raising-bad-type
+
 _LOCAL_PATH = ".srm/config"
 
+_GLOBAL_PATH = "~/.srmconfig"
+_GLOBAL_KEYS = {'my.temp.key'}
 
-class Conf:
-    """Config store."""
 
-    def __init__(self, path: str, parent: Optional['Conf']=None) -> None:
-        path = os.path.expanduser(path)
-        self.path = os.path.abspath(path)
-        self.parent = parent
+class Conf(abc.MutableMapping):
+    """Config store, backed by a TOML formatted file."""
+
+    def __init__(self, path: str, valid_keys: Optional[Set[str]] = None) -> None:
+        """
+        :param path: Relative or absolute path of the config file to use.
+        :param valid_keys: When defined, __setitem__ will reject key that are not found in this
+                           iterable.
+        """
+        self._path = os.path.expanduser(path)
+        self._valid_keys = valid_keys
+        self._toml_dict = {}  # type: Dict[str,Any]
 
     def exists(self) -> bool:
         """Return True if config exists."""
-        return os.path.exists(self.path) and os.path.isfile(self.path)
+        return os.path.exists(self._path) and os.path.isfile(self._path)
 
-    def load(self) -> None:
-        """Loads all data from the config file."""
-        pass
+    def load(self, create: bool = False) -> None:
+        """
+        Load all data from the config file.
+        :param create: When True, the path and config will be created instead of raising an
+                       exception.
+        """
+        try:
+            with open(self._path) as f:
+                self._toml_dict = toml.load(f)
+        except FileNotFoundError:
+            if create:
+                basedir = os.path.dirname(self._path)
+                os.makedirs(basedir)
+                open(self._path, 'w').close()
+            else:
+                raise
 
-    def save(self) -> None:
-        """Save all values to the config file."""
-        if self.parent:
-            self.parent.save()
+    def dump(self) -> None:
+        """Dump all values to the config file."""
+        with open(self._path, "w+") as f:
+            toml.dump(self._toml_dict, f)
 
-    def create(self) -> None:
-        """Create the config file and all necessary directory paths."""
-        pass
+    def __getitem__(self, k: str) -> Any:
+        return iterutils.get_path(self._toml_dict, k)
+
+    def __setitem__(self, k: str, v: Any) -> None:
+        if self._valid_keys and k not in self._valid_keys:
+            raise KeyError(f"Key {k} is not allowed")
+
+        nested_tables, k = Conf._extract_table_list(k)
+        table = self._toml_dict
+        for i in nested_tables:
+            table = table.setdefault(i, dict())
+        table[k] = v
+
+    def __delitem__(self, k: str) -> None:
+        nested_tables, k = Conf._extract_table_list(k)
+        table = iterutils.get_path(self._toml_dict, nested_tables)
+        del table[k]
+
+    def __iter__(self) -> Iterator:
+        raise NotImplemented
+
+    def __len__(self) -> int:
+        raise NotImplemented
+
+    @staticmethod
+    def _extract_table_list(k: str) -> Tuple[List[str], str]:
+        tables = k.split('.')
+        return tables, tables.pop(-1)
+
+
+# Callable that will always return the global config.
+GlobalConf = funcutils.partial(Conf, _GLOBAL_PATH, _GLOBAL_KEYS)  # pylint: disable=invalid-name
+
+
+class LocalConf(Conf):
+    """
+    Load and store keys to a local TOML config file. When a key is not in the local configuration,
+    defer to the global TOML file.
+    """
+
+    def __init__(self, path: Optional[str] = None) -> None:
+        super().__init__(path or _LOCAL_PATH)
+        self._global = GlobalConf()
+
+    def load(self, create: bool = False) -> None:
+        super().load(create)
+        self._global.load(create)
+
+    def dump(self) -> None:
+        super().dump()
+        self._global.dump()
+
+    def __getitem__(self, k: str) -> Any:
+        try:
+            return super().__getitem__(k)
+        except KeyError:
+            return self._global.__getitem__(k)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,125 @@
+import os
 import tempfile
+import unittest.mock
 
-from srm.config import Conf
+import pytest
+import toml
+
+from srm.config import Conf, GlobalConf, LocalConf
 
 
-def test_config_exists():
+def test_config_not_exists():
     with tempfile.TemporaryDirectory() as d:
         c = Conf(d)
         assert not c.exists()
 
+
+def test_config_exists():
     with tempfile.NamedTemporaryFile() as t:
         c = Conf(t.name)
         assert c.exists()
+
+
+def test_load_create_with_path():
+    with tempfile.TemporaryDirectory() as d:
+        c = Conf(os.path.join(d, "a/b/file.txt"))
+        c.load(create=True)
+        assert c.exists()
+
+
+def test_set_and_get_key():
+    c = Conf('cofig')
+    c['key'] = 10
+    assert c['key'] == 10
+
+
+def test_set_key_not_in_whitelist_raises_exception():
+    c = Conf('cofig', valid_keys=['field_ok'])
+    with pytest.raises(KeyError):
+        c['field_bad'] = 10
+
+
+def test_set_and_del_key():
+    c = Conf('config')
+    c['key'] = 10
+    del c['key']
+    with pytest.raises(KeyError):
+        c['key']
+
+
+def test_set_and_get_nested_key():
+    c = Conf('config')
+    c['s1.s2.key'] = 10
+    assert c['s1.s2.key'] == 10
+
+
+def test_set_and_del_nested_key():
+    c = Conf('config')
+    c['s1.s2.key'] = 10
+    del c['s1.s2.key']
+    with pytest.raises(KeyError):
+        c['s1.s2.key']
+
+
+def test_dump_config():
+    with tempfile.NamedTemporaryFile() as t:
+        c = Conf(t.name)
+        c['key'] = 10
+        c.dump()
+        with open(t.name) as f:
+            assert f.readline() == 'key = 10\n'
+
+
+def test_dump_nested_config():
+    with tempfile.NamedTemporaryFile() as t:
+        c = Conf(t.name)
+        c['s1.s2.key'] = 10
+        assert 's1.s2.key' in c
+        c.dump()
+        with open(t.name) as f:
+            assert f.readlines() == ['[s1.s2]\n', 'key = 10\n']
+
+
+def test_load_config():
+    with tempfile.NamedTemporaryFile() as t:
+        with open(t.name, 'w+') as f:
+            toml.dump({'key1': 1, 'key2': 2}, f)
+        c = Conf(t.name)
+        c.load()
+        assert 'key2' in c
+        assert c['key2'] == 2
+
+
+def test_dump_local_config():
+    with tempfile.NamedTemporaryFile() as t:
+        c = LocalConf(t.name)
+        c['key'] = 10
+        c.dump()
+        with open(t.name) as f:
+            assert f.readline() == 'key = 10\n'
+
+
+def test_load_local_config():
+    with tempfile.NamedTemporaryFile() as t:
+        with open(t.name, 'w+') as f:
+            toml.dump({'key1': 1, 'key2': 2}, f)
+        c = LocalConf(t.name)
+        c.load()
+        assert 'key2' in c
+        assert c['key2'] == 2
+
+
+@unittest.mock.patch('srm.config.GlobalConf', autospec=True)
+def test_get_global_key_from_local_config(gconf_cls):
+    with tempfile.NamedTemporaryFile() as global_path:
+        # set a key/value into mock global config
+        gconf = Conf(global_path.name)
+        gconf_cls.return_value = gconf
+        gconf['key'] = 10
+        gconf.dump()
+        # test
+        with tempfile.NamedTemporaryFile() as local_path:
+            c = LocalConf(local_path.name)
+            c.load()
+            assert 'key' in c
+            assert c['key'] == 10
